79a2f235ebda8ba4e6a778a724d8a386ca24e6d9
Benoit Vasseur <benoit.vasseur@zenika.com>
2019-10-31 14:07:55 +0100

    [WIP] simple form path

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FormEvent, useState, SyntheticEvent } from "react";
 
-const App: React.FC = () => {
+interface QuestionAndAnswers {
+  [question: string]: {
+    [answser: string]: QuestionAndAnswers | string | null;
+  };
+}
+
+const App: React.FC<{ questionAndAnswers: QuestionAndAnswers }> = ({
+  questionAndAnswers
+}) => {
+  const [currentQuestionAndAnswers, setCurrentQuestionAndAnswsers] = useState(
+    questionAndAnswers
+  );
+  if (currentQuestionAndAnswers === null) {
+    return <p>Merci d'avoir rempli le questionnaire</p>;
+  }
+  const question = Object.keys(currentQuestionAndAnswers)[0];
+  const answsers = Object.keys(currentQuestionAndAnswers[question]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello form</h1>
+      <form onSubmit={onSubmit}>
+        <Question question={question} answers={answsers} />
+        <div>
+          <input type="submit" value="Ok" />
+        </div>
+      </form>
+    </div>
+  );
+
+  function onSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const answerElt = event.currentTarget["answer"] as HTMLInputElement;
+    setCurrentQuestionAndAnswsers(currentQuestionAndAnswers[question][
+      answerElt.value
+    ] as QuestionAndAnswers);
+  }
+};
+
+function Question({
+  question,
+  answers
+}: {
+  question: string;
+  answers: string[];
+}) {
+  return (
+    <div>
+      <p>{question}</p>
+      {answers.map(answer => (
+        <>
+          <input key={"answer" + answer} type="radio" value={answer} name="answer" id={answer} />
+          <label key={"label_"+answer} htmlFor={answer}>{answer}</label>
+        </>
+      ))}
     </div>
   );
 }
---
+++ src/questions.tsx
@@ -0,0 +1,24 @@
+export const questions = {
+  "Comment allez-vous ?": {
+    "Au Top !": {
+      "Cool, qu'est-ce qui fait que tu es au top :) ?": {
+        "Le perso": null,
+        "Le boulot": null
+      }
+    },
+    "Pas glop": {
+        "Arf, on peut te soutenir / t'aider ?": {
+          "Oui avec du financement": {
+            "De combien ?": {
+              "< 1000": null,
+              "< 5000": null,
+              "> 5000": null
+            }
+          },
+          "Oui avec du temps": null,
+          "Non": null,
+          Autre: "__text_input__"
+        }
+      }
+  }
+};
